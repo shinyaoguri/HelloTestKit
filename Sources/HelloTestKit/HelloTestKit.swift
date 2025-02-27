5a823e9a4a3fecf75f75c5c8b9f8935cce472e45
shinyaoguri <ogrium@gmail.com>
2025-02-27 18:58:14 +0900

    update

--- Sources/HelloTestKit/HelloTestKit.swift
+++ Sources/HelloTestKit/HelloTestKit.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-class HelloTestKit {
+public class HelloTestKit {
     func hello() -> String {
         return "Hello, World!"
     }
